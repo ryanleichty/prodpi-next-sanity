c605c088ebe4222d3da4bf7671863da1df4cddec
Ryan Leichty <ryan.leichty@gmail.com>
2024-05-16 12:19:57 -0500

    Update sanity related packages

--- sanity.config.ts
+++ sanity.config.ts
@@ -29,7 +29,9 @@ export default defineConfig({
     }),
     media(),
     presentationTool({
-      locate,
+      resolve: {
+        locations: locate,
+      },
       icon: EyeOpenIcon,
       previewUrl: {
         draftMode: {
