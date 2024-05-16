e079f8dd6015733e9e0c7d27d4abb751b333786d
Ryan Leichty <ryan.leichty@gmail.com>
2024-05-16 09:46:57 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { cx } from '@/utils'
 
 type Props = React.ComponentPropsWithoutRef<'footer'>
 
-const footerLinks = [
+const FOOTER_LINKS = [
   {
     title: 'Turnaround Times',
     url: '/turnaround-times',
@@ -23,6 +23,21 @@ const footerLinks = [
   },
 ]
 
+const CONTACT_INFO = [
+  {
+    title: '612-324-8692',
+    url: 'tel:612-324-8692',
+  },
+  {
+    title: '1-855-776-3745',
+    url: 'tel:1-855-776-3745',
+  },
+  {
+    title: 'support@prodpi.com',
+    url: 'mailto:support@prodpi.com',
+  },
+]
+
 export default function Footer({ className, ...props }: Props) {
   return (
     <>
@@ -35,7 +50,7 @@ export default function Footer({ className, ...props }: Props) {
             <div>
               <h2 className="mb-4 font-sans-wide">Helpful Info</h2>
               <ul>
-                {footerLinks.map((item) => (
+                {FOOTER_LINKS.map((item) => (
                   <li key={item.url}>
                     <Link className="inline-block py-0.5" href={item.url}>
                       {item.title}
@@ -56,25 +71,14 @@ export default function Footer({ className, ...props }: Props) {
             </div>
             <div>
               <h2 className="mb-4 font-sans-wide">Contact Us</h2>
-              <ul>
-                <li>
-                  <a className="inline-block py-0.5" href="tel:612-324-8692">
-                    612-324-8692
-                  </a>
-                </li>
-                <li>
-                  <a className="inline-block py-0.5" href="tel:1-855-776-3745">
-                    1-855-776-3745
-                  </a>
-                </li>
-                <li>
-                  <a className="inline-block py-0.5" href="mailto:support@prodpi.com">
-                    support@prodpi.com
-                  </a>
-                </li>
-              </ul>
+              {CONTACT_INFO.map((item) => (
+                <p key={item.url}>
+                  <Link className="inline-block py-0.5" href={item.url}>
+                    {item.title}
+                  </Link>
+                </p>
+              ))}
             </div>
-            <div></div>
           </div>
           <div className="border-t border-sand/50 pt-8 text-sm text-sand/50">
             Copyright 2023 © ProDPI. All rights reserved. Privacy Policy
